09293331b793de0383d2c9754a367bc9834c1b05
phoebeCauld <vikaperova291013@gmail.com>
2021-10-03 17:12:59 +0300

    create saveItem and loadItem methods instead of UserDefaults using NSCoder

---
+++ ToDo/Item.swift
@@ -0,0 +1,14 @@
+//
+//  list.swift
+//  ToDo
+//
+//  Created by F1xTeoNtTsS on 03.10.2021.
+//
+
+
+import UIKit
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
