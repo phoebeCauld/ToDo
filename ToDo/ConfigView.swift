c740ea0d11ecde2771a1577a5d5ec252cfb09eff
phoebeCauld <vikaperova291013@gmail.com>
2021-10-05 12:34:41 +0300

    set up core data and create search bar

--- ToDo/ConfigView.swift
+++ ToDo/ConfigView.swift
@@ -9,13 +9,26 @@ import UIKit
 
 struct ConfigTableViewController {
     
+    let searchField: UISearchController = {
+       let sb = UISearchController(searchResultsController: nil)
+        sb.searchBar.searchBarStyle = .prominent
+        sb.searchBar.placeholder = " Search..."
+        sb.searchBar.sizeToFit()
+        sb.searchBar.searchTextField.backgroundColor = .white
+        return sb
+    }()
+    
     func navigationSetup(_ tableView: UITableViewController){
+        tableView.view.backgroundColor = .white
+        tableView.navigationItem.searchController = searchField
+        searchField.hidesNavigationBarDuringPresentation = false
+        searchField.obscuresBackgroundDuringPresentation = false
+        tableView.navigationItem.hidesSearchBarWhenScrolling = false
+        
         tableView.navigationItem.title = "ToDo"
         tableView.navigationController?.navigationBar.barTintColor = .systemBlue
         tableView.navigationController?.navigationBar.tintColor = .white
         tableView.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20, weight: .black), NSAttributedString.Key.foregroundColor: UIColor.white]
-        
-        
     }
 
 
